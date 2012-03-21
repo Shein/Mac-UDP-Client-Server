1af6fb84b905b15c33e32a5c41877e47d5a79934
Daniel Shein <shein@loftdev.com>
2012-03-21 16:36:00 +0200

    Initial Commit

---
+++ UDPClientServer/UDPClientServer.h
@@ -0,0 +1,13 @@
+//
+//  UDPClientServer.h
+//  UDPClientServer
+//
+//  Created by Daniel Shein on 3/21/12.
+//  Copyright (c) 2012 LoFT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UDPClientServer : NSObject
+
+@end
---
+++ UDPClientServer/UDPClientServer.m
@@ -0,0 +1,13 @@
+//
+//  UDPClientServer.m
+//  UDPClientServer
+//
+//  Created by Daniel Shein on 3/21/12.
+//  Copyright (c) 2012 LoFT. All rights reserved.
+//
+
+#import "UDPClientServer.h"
+
+@implementation UDPClientServer
+
+@end
