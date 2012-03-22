590bd161edfe0da4d83a453ed808747ac8a8969a
Daniel Shein <shein@loftdev.com>
2012-03-22 12:13:54 +0200

    Foundation for universal library ( mid-debugging iOS universal build script )

---
+++ UDPClientServer-iOS/UDPClientServer_iOS.m
@@ -0,0 +1,13 @@
+//
+//  UDPClientServer_iOS.m
+//  UDPClientServer-iOS
+//
+//  Created by Daniel Shein on 3/22/12.
+//  Copyright (c) 2012 LoFT. All rights reserved.
+//
+
+#import "UDPClientServer_iOS.h"
+
+@implementation UDPClientServer_iOS
+
+@end
